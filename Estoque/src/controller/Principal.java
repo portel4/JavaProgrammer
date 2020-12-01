aa9d0e04efbb6dd4e9a813937df66c4af9d4967e
rdrud <rdrud@DrudiBook>
2020-12-01 11:38:59 -0300

    Classe Produto e Classe Principal

---
+++ Estoque/src/controller/Principal.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.Produto;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		geraProdutos();
+		listaProdutos();
+	}
+	
+	private static void geraProdutos() {
+		new Produto(1,"Teclado Microsoft Natural",15,180);
+		new Produto(2,"Monitor HP IPS Full HD 23,5",5,1000);
+		new Produto(3,"SSD Sandisk 480GB M2",10,500);
+		new Produto(4,"Mouse Logitech Gamer",15,100);
+		new Produto(5,"Memória DDR4 2666Mhz 8GB",20,300);
+		new Produto(6,"Placa NVidia RTX3080 Nova",5,3000);
+	}
+	
+	private static void listaProdutos() {
+		Produto.getLista().forEach(e -> System.out.println(e));
+	}
+	
+}
--- Estoque/src/model/Produto.java
+++ Estoque/src/model/Produto.java
@@ -1,5 +1,66 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Produto {
+	
+	private int codigo;
+	private String nome;
+	private int qtde;
+	private double valor;
+	private static List<Produto> lista = new ArrayList<Produto>();
+	
+	public static List<Produto> getLista() {
+		return lista;
+	}
+
+	public Produto() {
+		this(0,"",0,0);
+	}
+	public Produto(String nome, int qtde, double valor) {
+		this(0,nome,qtde,valor);
+	}
+	public Produto(int codigo, String nome, int qtde, double valor) {
+		setCodigo(codigo);
+		setNome(nome);
+		setQtde(qtde);
+		setValor(valor);
+		lista.add(this);
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQtde() {
+		return qtde;
+	}
+	public void setQtde(int qtde) {
+		if (qtde < 0) {
+			throw new IllegalArgumentException("Quantidade deve ser maior ou igual a zero");
+		} else {
+			this.qtde = qtde;
+		}
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	@Override
+	public String toString() {
+		return (getNome() + ": \t\tSaldo=" + getQtde() + "\t\tValor=" + getValor());
+	}
 
 }
